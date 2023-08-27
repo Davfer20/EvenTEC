ed3aa340b2645045dfa89ec77ebe85e3e6e1e501
whiit <ncqueescribir@gmail.com>
2023-08-27 17:40:25 -0600

    main screen update

--- javascript/indexActons.js
+++ javascript/indexActons.js
@@ -1,6 +1,18 @@
+let currentOption = 0
+let loginRefs = ["../html/loginE.html", "../html/loginA.html", "../html/loginC.html"]
+let singinRefs = ["../html/signinE.html", "../html/signinA.html", "../html/signinC.html"]
 
-function redirectionColaboradores(){
-  location.href = "../html/loginC.html"
+
+function selectMethod(n){
+  currentOption = n
+}
+
+function loginButton(){
+  location.href = loginRefs[currentOption]
+}
+
+function singinButton(){
+  location.href = singinRefs[currentOption]
 }
 
 
